f89f36db3f6c07afd1bcde63b933c010390ee1ea
Philipp Giese <giese.philipp+git@gmail.com>
2016-08-03 17:14:24 +0200

    added node connector example

---
+++ node/data-source/server.js
@@ -0,0 +1,46 @@
+const _ = require("lodash");
+const express = require("express");
+const app = express();
+
+const descriptor = require("./descriptor");
+const data = require("./data");
+
+app.get("/", function(req, res) {
+    res.json(descriptor);
+});
+
+app.get("/:type/options", function(req, res) {
+    const type = req.params.type;
+
+    if(!data[type]) {
+        return res.status(404).send();
+    }
+
+    res.json(_.map(data[type], function(value) {
+        return {
+            id: value.id,
+            name: value.fullName,
+        };
+    }));
+});
+
+app.get("/:type/:id", function(req, res) {
+    const type = req.params.type;
+    const id = req.params.id;
+
+    if(!data[type]) {
+        return res.status(404).send();
+    }
+
+    const resource = data[type][id];
+
+    if(!resource) {
+        return res.status(404).send();
+    }
+
+    res.json(resource);
+});
+
+app.listen(5000, function() {
+    console.log("Data source listening on port 5000.");
+});
