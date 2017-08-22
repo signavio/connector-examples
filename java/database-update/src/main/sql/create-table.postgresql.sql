da57307e4bed9494fb50193ad16dd7c3cf42fba4
Peter Hilton <hilton@users.noreply.github.com>
2017-08-22 12:30:18 +0100

    Fix SQL DDL

--- java/database-update/src/main/sql/create-table.postgresql.sql
+++ java/database-update/src/main/sql/create-table.postgresql.sql
@@ -1,5 +1,5 @@
 create table if not exists case_event (
-    id serial primary key ,
+    id serial primary key,
     case_id varchar(24) not null,
     created timestamp not null,
     type varchar(16));
