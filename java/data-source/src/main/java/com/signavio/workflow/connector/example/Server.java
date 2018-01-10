79b10b667ba17392ff65f29be51cfed08d8ffd34
Peter Hilton <hilton@users.noreply.github.com>
2018-01-10 16:57:58 +0100

    Consolidate logging

--- java/data-source/src/main/java/com/signavio/workflow/connector/example/Server.java
+++ java/data-source/src/main/java/com/signavio/workflow/connector/example/Server.java
@@ -1,14 +1,12 @@
 package com.signavio.workflow.connector.example;
 
-import com.signavio.workflow.connector.example.customer.Customer;
-import com.signavio.workflow.connector.example.customer.CustomerOption;
-import com.signavio.workflow.connector.example.customer.CustomerService;
+import java.io.IOException;
+
+import com.signavio.workflow.connector.example.customer.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import spark.*;
 import spark.utils.IOUtils;
-
-import java.io.IOException;
 
 import static spark.Spark.*;
 
@@ -45,14 +43,26 @@ public class Server {
     get("/customer/options/:id", Server::serveOption, responseTransformer);
     get("/customer/:id", Server::serveRecord, responseTransformer);
 
-    // Map a Java exception type to an HTTP response status.
-    exception(NotFoundException.class, (error, request, response) -> {
-      response.status(404);
+    // Log requests.
+    before((request, response) -> {
+      LOGGER.info("→ {} {}", request.requestMethod(), request.uri());
     });
 
     // Define the response content type for all endpoints.
     after((request, response) -> {
       response.type("application/json;charset=UTF-8");
+    });
+
+    // Log responses.
+    afterAfter((request, response) -> {
+      LOGGER.info("← HTTP {}", response.status());
+    });
+
+    // Map a Java exception type to an HTTP response status.
+    exception(NotFoundException.class, (error, request, response) -> {
+      response.status(404);
+      response.type("application/json;charset=UTF-8");
+      response.body("{}");
     });
   }
 
@@ -60,28 +70,24 @@ public class Server {
    * Serves <a href="https://docs.signavio.com/userguide/workflow/en/integration/connectors.html#record-details">Record details</a>.
    */
   private static Object serveRecord(Request request, Response response) throws NotFoundException {
-      String id = request.params("id");
-      LOGGER.info("Fetching customer with ID: " + id);
-      Customer customer = customerService.find(id);
-      if (customer == null) {
-        LOGGER.info("Did not find customer.");
-        throw new NotFoundException();
-      }
-      return customer;
+    String id = request.params("id");
+    Customer customer = customerService.find(id);
+    if (customer == null) {
+      throw new NotFoundException();
+    }
+    return customer;
   }
 
   /**
    * Serves a <a href="https://docs.signavio.com/userguide/workflow/en/integration/connectors.html#record-type-option-single-option">Record type option (single option)</a>.
    */
   private static Object serveOption(Request request, Response response) throws NotFoundException {
-      String id = request.params("id");
-      LOGGER.info("Fetching customer option with ID: " + id);
-      CustomerOption option = customerService.findOneOption(id);
-      if (option == null) {
-        LOGGER.info("Did not find option.");
-        throw new NotFoundException();
-      }
-      return option;
+    String id = request.params("id");
+    CustomerOption option = customerService.findOneOption(id);
+    if (option == null) {
+      throw new NotFoundException();
+    }
+    return option;
   }
 
   /**
@@ -93,6 +99,5 @@ public class Server {
   }
 
   private static class NotFoundException extends Exception {
-
   }
 }
