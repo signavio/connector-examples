6a4b7f9645956b9d39ea24c7d37e8ca50067e1f8
Christian Wiggert <SilentGert@users.noreply.github.com>
2018-02-08 09:24:36 +0100

    added missing parentheses

--- node/data-source/src/server.js
+++ node/data-source/src/server.js
@@ -5,24 +5,24 @@ const app = express();
 const descriptor = require("./descriptor");
 const data = require("./data");
 
-app.get("/", function(req, res) {
+app.get("/", function (req, res) {
     res.json(descriptor);
 });
 
-app.get("/:type/options", function(req, res) {
+app.get("/:type/options", function (req, res) {
     const type = req.params.type;
 
-    if(!data[type]) {
+    if (!data[type]) {
         return res.status(404).send();
     }
 
     const query = req.query.filter || "";
 
-    const options = _.filter(data[type], function(value) {
+    const options = _.filter(data[type], function (value) {
         return _.includes(value.fullName.toLowerCase(), query.toLowerCase());
     });
 
-    res.json(_.map(options, function(value) {
+    res.json(_.map(options, function (value) {
         return {
             id: value.id,
             name: value.fullName,
@@ -30,43 +30,43 @@ app.get("/:type/options", function(req, res) {
     }));
 });
 
-app.get("/:type/options/:id", function(req, res) {
+app.get("/:type/options/:id", function (req, res) {
     const type = req.params.type
     const id = req.params.type
 
-    if(!data[type]) {
+    if (!data[type]) {
         return res.status(404).send();
     }
 
     const resource = data[type][id];
 
-    if(!resource) {
+    if (!resource) {
         return res.status(404).send();
     }
 
     res.json({
         id: resource.id,
         name: resource.fullName,
-    }));
+    });
 })
 
-app.get("/:type/:id", function(req, res) {
+app.get("/:type/:id", function (req, res) {
     const type = req.params.type;
     const id = req.params.id;
 
-    if(!data[type]) {
+    if (!data[type]) {
         return res.status(404).send();
     }
 
     const resource = data[type][id];
 
-    if(!resource) {
+    if (!resource) {
         return res.status(404).send();
     }
 
     res.json(resource);
 });
 
-app.listen(5000, function() {
+app.listen(5000, function () {
     console.log("Data source listening on port 5000.");
 });
